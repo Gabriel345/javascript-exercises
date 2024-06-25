7c310b769d500986af6937dc67a35f93269c69ba
Gabriel345 <gabrielmzenerato242@gmail.com>
2024-06-25 07:52:19 -0300

    ex 3 complete
    duas soluções encontradas

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+// minha solução
+    // resultado='';
+// for(let i = string.length - 1; i >= 0; i--){
+//     resultado +=string[i];
+// }
+// return resultado;
 
+return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
